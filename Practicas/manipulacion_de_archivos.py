8da64e0c0226df2aceef81743405af2fca6b4984
JoaquinRestahinoch <restahinoch@gmail.com>
2023-04-10 00:01:07 -0300

    agregar ejercicios

--- Practicas/manipulacion_de_archivos.py
+++ Practicas/manipulacion_de_archivos.py
@@ -1,18 +1,41 @@
-def start_with(letra, archivo):
+#Ejercicio 1
+def letra(letra, archivo):
     contador = 0
     with open (archivo, "r") as file:
         for line in file:
-            if (line[0] != letra.lower() or line [0] != letra.upper ()):
-                count +=1
-    print("El númer de líneas que no empiezan con" , letra, "es", count)
+            if (line[0] != letra.lower() and line [0] != letra.upper ()):
+                contador +=1
+    print("El número de líneas que no empiezan con" , letra, "es", contador)
 
-start_with("H", "documento")
+letra("h", "mda.txt")
+
+#Ejercicio 2
+def linea(qlineas, archivo):
+    with open (archivo, "r") as file:
+        lineas = file.readlines()
+        for i in lineas[:qlineas]:
+            print(i, end = "")
+
+linea(2, "mda.txt")
 
 #Ejercicio 3
 
-def read_n_back_lines (n, archivo):
-    texto = open (archivo, "r").readlines()
-    for i in range ((len(texto) -n), len(texto)):
-        print(texto)
+def lineas(ultimas,archivo):
+    with open (archivo, "r") as file:
+        lineas3 = file.readlines()
+        ulineas = lineas3[-ultimas:]
+        for linea in ulineas:
+            print(linea, end="")
+lineas(2,"mda.txt")
 
-print(read_n_back_lines(4,"documento"))
+#Ejercicio 4
+
+def palabras(archivo):
+    with open (archivo, "r") as file:
+        lineas4 = file.readlines()
+        contador = 0
+        for i in lineas4:
+            contador+=1
+        print()
+        print(contador)
+palabras("mda.txt")
