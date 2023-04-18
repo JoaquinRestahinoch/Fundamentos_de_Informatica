3d0e4fb255e7a91e9464b777eeb41b8a7e1d7af7
JoaquinRestahinoch <restahinoch@gmail.com>
2023-04-18 11:58:08 -0300

    parte 2

--- Practicas/Python_Parte2.py
+++ Practicas/Python_Parte2.py
@@ -1,3 +1,4 @@
+"""
 #Ejercicio 1
 def procedimiento (lista):
     for i in lista :
@@ -29,8 +30,94 @@ print(posicion("Chau"))
 
 #Ejercicio 4
 
-def metodo (lista4,lista5,elemento):
-     return
-
+#Metodo 1
+from random import randrange
+def metodo (lista,lista2):
+     posicion = randrange(len(lista))
+     print(posicion)
+     elemento = lista[posicion]
+     lista2.append(elemento)
+     lista.remove(elemento)
+     print(lista)
+     print(lista2)
 lista4 = ["Hola", "Chau", "Buenas","Dias"] 
 lista5 = [1,2,3,4,5]
+print(metodo(lista4,lista5))
+
+# Metodo 2
+def metodo (lista,lista2,elemento):
+     lista2.append(elemento)
+     lista.remove(elemento)
+     print(lista)
+     print(lista2)
+lista4 = ["Hola", "Chau", "Buenas","Dias"] 
+lista5 = [1,2,3,4,5]
+print(metodo(lista4,lista5, "Chau"))
+
+#Ejercicio 5
+
+def booleanos(lista):
+    lista2 = []
+    for i in lista:
+        if i % 2 == 0 :
+            lista2.append("True")
+        else:
+             lista2.append("False")
+    return lista2
+lista6 = [1,2,3,4,5,6]
+print(booleanos(lista6)) 
+
+#Ejercicio 6
+
+def dic(cadena):
+    
+    diccionario = {}
+    for i in cadena:
+         pass
+    return diccionario
+print(dic("Holaaa"))
+
+#Ejercicio 7
+
+#Ejercicio 8
+
+def palindromo (palabra):
+     lista = []
+     lista.append(palabra)
+     inversa = palabra[::-1]
+     lista.append(inversa)
+     if lista [0] == lista[1]:
+          return True
+     else:
+          return False
+print(palindromo("argra"))
+
+#Ejercicio 9
+
+def productoria(numeros):
+    producto = 1
+    for i in numeros:
+         producto *= i
+    return producto
+numeros1 = [2,3,1,2]
+print(productoria(numeros1))
+"""
+#Ejercicio 10
+
+def club():
+     dic = {1: {"nombre_y_apellido": "Florencia Ocampo", "fecha_de_ingreso": "14092001", "cuota_al_dia": True},
+            2: {"nombre_y_apellido": "David Estévez", "fecha_de_ingreso": "14092001", "cuota_al_dia": True},
+            3: {"nombre_y_apellido": "Sofía Cáceres", "fecha_de_ingreso": "14092001", "cuota_al_dia": True}}
+     return dic
+dic = club()
+print(club)
+
+def cantidad_socios(dic):
+    socios = 0
+    for i in dic.values():
+        if "nombre_y_apellido" in i:
+            socios+=1
+    return socios
+print(cantidad_socios)
+          
+
